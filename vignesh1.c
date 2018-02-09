475d3885af5e8cc6e0b1991640108e924c5536a6
VigneshVkl <35764577+VigneshVkl@users.noreply.github.com>
2018-02-09 14:02:45 +0530

    Create vignesh1.c

---
+++ vignesh1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,f=1;
+	printf("enter any number\n");
+	scanf("%d",&a);
+	for(b=1;b<=a;b++)
+	f=f*a;
+	printf("the factorial %d is of %d a,f);
+	return 0;
+	
+}
