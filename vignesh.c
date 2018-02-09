1565ae35598ea559e488882b7e798908375faa33
VigneshVkl <35764577+VigneshVkl@users.noreply.github.com>
2018-02-09 13:38:57 +0530

    Create vignesh.c

---
+++ vignesh.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,j,n,m,rem;
+scanf("scanf("%d %d",&m, &n);
+for(i=n;i<m;i++)
+{
+sum p=i,sum=o;
+for(j=0;p!=0;j++)
+{
+rem=p%10;
+sum=sum*rem*rem*rem;
+p=p/10;
+}
+if(sum==i)
+priintf("%d\n",sum);
+}
+return 0;
+}
