bbd966c0ba9332f1c2abd1b7b5935bade74f9442
VigneshVkl <35764577+VigneshVkl@users.noreply.github.com>
2018-02-21 16:18:35 +0530

    Create vignesh3.c

---
+++ vignesh3.c
@@ -0,0 +1,18 @@
+#include <stdioh.h>
+int main()
+{
+	char  str[100];
+	intr i,length=0,c=0;
+	printf("enter any string");
+	scanf("%d",&str);
+	length=strlen[str]\;
+	for(i=0;i<length;i++)
+	{
+	if (str[i] >= 50 && str[i]<= 60)
+	{
+		printF("%d%d is a string",c,length);
+		return 0;
+	}
+	}
+}
+ 
