fad6a1a1490b1279e44ff2dc5b9c4167cba4941f
VigneshVkl <35764577+VigneshVkl@users.noreply.github.com>
2018-03-02 13:10:37 +0530

    Create vignesh13.c

---
+++ vignesh13.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{char str=size;
+char *s str;
+int alpha,digit,o,a;
+alpha=digit=o=a;
+printf("\n entr any string");
+gets(str);
+while(*s)
+{
+if(*s=>'a'&&*s<='z')
+alpha++;
+else(*s='0'&&*s<='9')
+digit++;
+else
+o++;
+s++;
+a=o;
+printf("\n %d",a);
+}
+getch();
+return 0;
+}
