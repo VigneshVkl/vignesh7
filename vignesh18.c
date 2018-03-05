f58ad7c36da4750af3ab3cc4a021d4572a22ba7a
VigneshVkl <35764577+VigneshVkl@users.noreply.github.com>
2018-03-05 15:59:00 +0530

    Create vignesh18.c

---
+++ vignesh18.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char str;
+	int k;
+	printf("\n enter the string");
+	scanf("%s",&str);
+	printf("\n enter the value of k");
+	scanf("%d",&k);
+	for(i=0;i<k;i++)
+	{
+		printf("\n the string are %s",i);
+	getch();
+	return 0;
+	}
+}
