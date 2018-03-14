d04af56cc678035858068ac4cf95b5a33c35ce60
VigneshVkl <35764577+VigneshVkl@users.noreply.github.com>
2018-03-14 16:30:44 +0530

    Create vignesh31.c

---
+++ vignesh31.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("%d",a-2);
+	}
+	else
+	{
+		printf("%d",a-1);
+	}
+	getch();
+  return 0
+  }
