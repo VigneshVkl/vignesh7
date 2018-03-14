3a5742df3b521da8ac5b9f8420344540761ff0ae
VigneshVkl <35764577+VigneshVkl@users.noreply.github.com>
2018-03-14 16:37:08 +0530

    Create vignesh32.c

---
+++ vignesh32.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n;
+clrscr();
+printf("\n enter the two numbers");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("\n the number is even");
+}
+else
+{
+printf("\n the number is odd");
+}
+return 0;
+}
